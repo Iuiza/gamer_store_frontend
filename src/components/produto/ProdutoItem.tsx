7658abf096605c0a9248a945e825d3196aec9889
Iuiza <luiza.floquinho@gmail.com>
2024-09-30 23:56:45 -0300

    updating page

---
+++ src/components/produto/ProdutoItem.tsx
@@ -0,0 +1,18 @@
+import { Produto } from '../../core'
+import Link from 'next/link'
+
+export interface ProdutoItemProps {
+    produto: Produto
+}
+
+export default function ProdutoItem(props: ProdutoItemProps) {
+    const {produto} = props
+    return(
+        <Link
+            href={`/produto/${props.produto.id}`}
+            className="flex flex-col bg-violet-dark border border-white/10 rounded-xl relative max-w-[350px]"
+        >
+            {produto.nome}
+        </Link>
+    )
+}
